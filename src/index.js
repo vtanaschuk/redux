f2fd7a8938eb78101eec57530dc7fe92072d13aa
vtanashchuk <vtanashchuk@immersetech.co>
2023-11-23 17:22:25 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+const initialState = 0;
+
+const reducer = (state= 0, action) =>{
+    if (action.type === 'INC'){
+        return state + 1;
+    }
+    if (action.type === 'DEC'){
+        return state - 1;
+    }
+    if (action.type === 'RND'){
+        return ( state * action.payload);
+    }
+    return state;
+}
+
+
+const store = createStore(reducer)
+const update = () =>{
+    document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update)
+
+document.getElementById('dec').addEventListener('click', ()=>{
+    store.dispatch({type: 'DEC'})
+})
+
+document.getElementById('inc').addEventListener('click', ()=>{
+    store.dispatch({type: 'INC'})
+})
+
+document.getElementById('rnd').addEventListener('click', ()=>{
+    const value = Math.floor(Math.random() * 10);
+    store.dispatch({type: 'RND', payload: value})
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <></>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
